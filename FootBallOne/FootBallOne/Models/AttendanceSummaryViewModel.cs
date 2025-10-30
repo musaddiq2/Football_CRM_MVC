f3e802ead79237f4a21c3525b57148a327327b39
Sayed Musaddiq <132586473+musaddiq2@users.noreply.github.com>
2025-10-30 12:43:07 +0530

    Delete FootBallOne/FootBallOne directory
    Old directory deleted

--- FootBallOne/FootBallOne/Models/AttendanceSummaryViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FootBallOne.Models;
-    public class AttendanceSummaryViewModel
-    {
-        public DateTime Date { get; set; }
-        public int TotalRecords { get; set; }
-        public int TotalPresent { get; set; }
-        public int TotalAbsent { get; set; }
-    }
-
